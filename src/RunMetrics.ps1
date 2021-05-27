b672d25497308f70f591e0f431983589bf6fda82
Arseniy Golovnev <Golovnev@BlackShadow.Name>
2021-05-27 15:14:17 +0300

    Initial commit

---
+++ src/RunMetrics.ps1
@@ -0,0 +1,19 @@
+﻿$artsPath = "C:\arts\";
+$exclude = [System.Environment]::GetEnvironmentVariable("METRICS_EXCLUDE");
+if ($exclude -eq $null) {
+    $excludedProjects = @();
+} else {
+    $excludedProjects = $exclude.Split(",");
+}
+
+$location = Get-Location;
+$projects = Get-ChildItem "*.csproj" -Recurse -Exclude $excludedProjects;
+foreach ($project in $projects) {
+    Set-Location $project.Directory;
+    dotnet add $project.Name package Microsoft.CodeAnalysis.Metrics
+    dotnet build $project.Name -t:Metrics
+    dotnet remove $project.Name package Microsoft.CodeAnalysis.Metrics
+    Move-Item ($project.BaseName + ".Metrics.xml") $artsPath -Force
+}
+
+Set-Location $location;
